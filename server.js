7dbd736e9f4cd2b6b19aa2dd9efbe24f4ff89ca8
cgtnt <tntmynamemetak@gmail.com>
2024-01-13 21:56:55 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express()
+
+//port for local testing
+const port = 8080
+
+app.set('view engine', 'ejs');
+
+//public folder is for all resources (css, js, images)
+app.use(express.static('public'))
+
+//default page is periodic table
+app.get("/",(req,res)=>{
+    res.render('pages/index')
+})
+
+app.get("/:elementId", (req,res)=>{ //dynamic routing for elements
+    //add checks if el exists and create json object with all necessary info
+    res.render('pages/elementTemplate', {elementId: req.params.elementId})
+})
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`)
+})
